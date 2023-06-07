77475de19dd5589544f0897f441025989a8ff1dc
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-07 05:46:44 -0700

    t9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def lastofdig(number):
-    sign = '-'
-    lastofdigit = abs(number) % 10
-    if number < 0 :
-        return sign + str(lastofdigit)
-    else:
-        return lastofdigit
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
+    return last_digit
