51f6bb716c246e00c460e9df47fc116c38763080
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-08 10:44:48 -0700

    t2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    x = sys.argv
+    y= len(x) - 1
+
+    if y==0:
+        print("{} arguments.".format(y))
+    elif y==1:
+        print("{} argument:".format(y))
+        print("{}: {}".format(y, x[1]))
+    else:
+        print("{} arguments:".format(y))
+        for i in range(1, len(x)):
+            print("{}: {}".format(i, x[i]))
