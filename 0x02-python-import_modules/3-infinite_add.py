86f18565e4c5b71d26bc6652ba5e917deb72d94f
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-08 11:33:51 -0700

    t3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-    x = sys.argv[1:]
-    y = 0
-    for i in x:
-        y += int(i)
-    print(y)
+if __name__ == '__main__':
+    ty = 0
+    i = 1
+    x = sys.argv
+    y = len(x) - 1
+    while i != len(x):
+        ty = ty + int(x[i])
+        i += 1
+    print(ty)
