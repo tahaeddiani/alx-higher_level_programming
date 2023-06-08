cd5cd9101cc2fe91e145f7ad9b2c408570877592
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-08 10:49:32 -0700

    t2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 if __name__ == "__main__":
     x = sys.argv
-    y= len(x) - 1
+    y = len(x) - 1
 
-    if y==0:
+    if y == 0:
         print("{} arguments.".format(y))
-    elif y==1:
+    elif y == 1:
         print("{} argument:".format(y))
         print("{}: {}".format(y, x[1]))
     else:
