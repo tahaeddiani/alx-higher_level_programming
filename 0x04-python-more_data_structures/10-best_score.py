3f6fb48894de7c510ccca2dfaba81ec2fba33e68
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-14 12:11:41 -0700

    t10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    best_key = max(a_dictionary, key=a_dictionary.get)
+    return best_key
