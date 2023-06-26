6ffc0f5a00ccd5a06f87b901998a4f08b83680f4
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-26 09:08:24 -0700

    t0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            cnt += 1
+        except:
+            break
+    print("")
+    return (cnt)
