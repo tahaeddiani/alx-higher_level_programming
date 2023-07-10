26c485bfe4e26ad530ce78ac07aeddbb885e9011
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-10 11:28:09 -0700

    t6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""emptyclass"""
+
+
+class BaseGeometry:
+    """improve class"""
+    def area(self):
+        """improve Geometry"""
+        raise Exception("area() is not implemented")
