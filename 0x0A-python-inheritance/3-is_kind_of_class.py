d246b056f446635d9e1083c771f2a5e51ba1dfd0
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-10 10:46:49 -0700

    t3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""same class or inherit fro"""
+
+
+def is_kind_of_class(obj, a_class):
+    """is kind of class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        False
