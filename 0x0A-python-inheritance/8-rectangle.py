e0ffaa78542b06ea0afbd48ea59f5a1f14e0f259
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-10 14:49:12 -0700

    t8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,14 +3,11 @@
 
 
 class Rectangle(BaseGeometry):
-	"""child class of basegeo"""
-	
-
-	def __init__(self, width, height):
-		"""rectangle"""
-
-		super().__init__()
-		self.integer_validator("width", width)
-		self.__width = width
+    """child class of basegeo"""	
+    def __init__(self, width, height):
+        """rectangle"""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
         self.__height = height
