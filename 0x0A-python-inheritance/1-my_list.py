f2dd2f309b039cfe6c1bab8d5ea9c99bf95ac001
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-10 10:09:27 -0700

    t1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""My list printed sorted """
+
+
+class MyList(list):
+    """ class MyList"""
+
+    def print_sorted(self):
+        """ class MyList"""
+        print(sorted(self))
