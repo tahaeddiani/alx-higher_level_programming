7cadfe03e59d23655201d87c703631dca246ade5
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-11 09:55:06 -0700

    t2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """append to file"""
 
+
 def append_write(filename="", text=""):
     """apend write"""
-    
+
     with open(filename, 'a', encoding="utf-8") as aw:
         aw.write(text)
         return len(text)
