1f7188ed7091b09f639508887124c11f0f2036a9
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-11 12:52:54 -0700

    t11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Create a Class  Student"""
+"""method for student creation"""
 
 
 class Student:
-    """ class to create a dict obj """
+    """Student obj"""
 
     def __init__(self, first_name, last_name, age):
-        """ initialization for Student object """
-        self.age = age
-        self.last_name = last_name
         self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """ retrieves a dictionary representation """
         return self.__dict__
