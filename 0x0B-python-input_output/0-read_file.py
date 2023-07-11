8fd6eed9b04ebfee4d22c94cefd95f0d4d1e68fb
tahaeddiani <tahaeddiani2@gmail.com>
2023-07-11 08:50:51 -0700

    t0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """read file"""
     with open('my_file_0.txt', 'r', encoding="utf-8") as f:
-        x = f.read()
-        print(x, end="")
+        print(f.read(), end="")
