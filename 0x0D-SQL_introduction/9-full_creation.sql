dcebc7f74660edefb9a93a47c7cc9a9249f1c374
tahaeddiani <tahaeddiani2@gmail.com>
2023-08-16 08:22:14 -0700

    t9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- full creation
-CREATE TABLE if not exists second_table (
+CREATE TABLE IF NOT EXISTS second_table(
 id INT,
 name VARCHAR(256),
-score INT,
+score INT
 );
-INSERT INTO (id,name,score) VALUES(1,'John',10);
-INSERT INTO (id,name,score) VALUES(2,'Alex',3);
-INSERT INTO (id,name,score) VALUES(3,'Bob',14);
-INSERT INTO (id,name,score) VALUES(4,'George',8);
+INSERT INTO second_table (id,name,score) VALUES(1,"John",10);
+INSERT INTO second_table (id,name,score) VALUES(2,"Alex",3);
+INSERT INTO second_table (id,name,score) VALUES(3,"Bob",14);
+INSERT INTO second_table (id,name,score) VALUES(4,"George",8);
