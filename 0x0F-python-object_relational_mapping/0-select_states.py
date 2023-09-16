cc39d8d6541650cf9aa5dc43bf66170b46c7e7ce
tahaeddiani <Tahaeddiani2@gmail.com>
2023-09-16 07:07:33 -0700

    t0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,14 @@
 import MySQLdb
 import sys
 
-vr = sys.argv
-dd = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=vr[1],
-        passwd=vr[2],
-        db=vr[3])
+if __name__ == "__main__":
+    vr = sys.argv
+    dd = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=vr[1],
+            passwd=vr[2],
+            db=vr[3])
 
 cr = dd.cursor()
 
