d5ea565e2dc1ff8bbba013552cf932b5268a2fa8
tahaeddiani <Tahaeddiani2@gmail.com>
2023-09-16 06:58:25 -0700

    t0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""get all states"""
+
+import MySQLdb
+import sys
+
+vr = sys.argv
+dd = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=vr[1],
+        passwd=vr[2],
+        db=vr[3])
+
+cr = dd.cursor()
+
+y = "SELECT * FROM states ORDER BY id ASC"
+cr.execute(y)
+
+ty = cr.fetchall()
+
+for i in ty:
+    print(i)
+
+dd.close()
